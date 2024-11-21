7319a0a8d3036611c749967c24e2f925f793d6e8
NatTHboi <supanatpansri@gmail.com>
2024-11-21 19:15:58 +0700

    Add files via upload

---
+++ HW4-1.cpp
@@ -0,0 +1,18 @@
+/*
+    จงแสดงผลตัวแปรทั้งหมดเรียงลำดับดังนี้ a b c d
+
+    Output:
+        10 20.0 Haha a
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a = 10 ;
+    float b = 20 ;
+    char c[] = "Haha" ;
+    char d = 'a' ;
+    printf( "%d %f %s %c", a, b, c, d) ; //TODO: #10 Display variable a, b, c and d.
+
+    return 0 ;
+}
