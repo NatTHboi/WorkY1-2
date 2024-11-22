913547351ef55b8ffe5c53072e9980be1f5ff17d
NatTHboi <supanatpansri@gmail.com>
2024-11-22 21:50:37 +0700

    Add files via upload

---
+++ HW4-6.cpp
@@ -0,0 +1,46 @@
+/*
+    เขียนโปรแกรมภาษาซีเพื่อรับข้อมูลพนักงานของบริษัทซอร์ฟแวร์ โดยรับข้อมูล รหัสประจำตัวพนักงาน , จำนวนชั่วโมงที่ทำงาน , รายได้ต่อชั่วโมง จากนั้นให้แสดงข้อมูลเลขประจำตัวพนักงาน พร้อมกับรายได้ทั้งหมดที่หนักงานจะได้รับทั้งหมด
+    (ผลลัพธ์ของ Salary จะต้องอยู่ในรูปแบบ Decimal Number เท่านั้น เช่น 374,000)
+
+    Test case:
+        Input the Employees ID(Max. 10 chars): 
+            0342
+        Input the working hrs: 
+            8
+        Salary amount/hr: 
+            15000
+    Output:
+        Expected Output:
+        Employees ID = 0342
+        Salary = U$ 120,000.00
+y8uin 
+    Test case:
+        Input the Employees ID(Max. 10 chars): 
+            0000500349
+        Input the working hrs: 
+            11
+        Salary amount/hr: 
+            34000
+    Output:
+        Expected Output:
+        Employees ID = 0000500349
+        Salary = U$ 374,000.00
+*/
+
+#include <stdio.h>
+
+int main() {
+    char ID[10] ;
+    int WH, Salary ;
+    printf( "Input the Employees ID(Max. 10 chars): \n") ;
+    scanf( "%s", ID ) ;
+    printf( "Input the working hrs: \n" ) ;
+    scanf( "%d", &WH ) ;
+    printf( "Salary amount/hr: \n" ) ;
+    scanf( "%d", &Salary ) ;
+    
+    printf( "Employees ID = %s\n", ID ) ;
+    printf( "Salary = U$ %d", WH * Salary ) ;
+
+    return 0 ;
+}
