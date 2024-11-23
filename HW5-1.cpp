41e8dba5d9372ec084d7114df5b0716513e37f23
NatTHboi <supanatpansri@gmail.com>
2024-11-23 23:42:50 +0700

    Add files via upload

---
+++ HW5-1.cpp
@@ -0,0 +1,24 @@
+/*
+    รับตัวอักขระมาทั้งหมด 4 ค่า แล้วนำมาแสดงผลย้อนกลับ เช่น กรอกค่า L M X Y ต้องแสดงเป็น Y X M L เป็นต้น
+    
+    Test case:
+        L M Y K
+    Output:
+        Result: K Y M L
+
+    Test case:
+        A B C D
+    Output:
+        Result: D C B A
+*/
+
+#include <stdio.h>
+
+int main() {
+    char a, b, c, d ;
+    printf( "Enter 4 character : \n" ) ;
+    scanf( "%c %c %c %c", &a, &b, &c, &d ) ;
+    printf( "Result : %c %c %c %c", d, c, b, a ) ;
+
+    return 0 ;
+}
