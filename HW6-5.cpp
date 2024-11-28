e019b64e1b5f8465b308637178bc1d2f81e8db5a
NatTHboi <supanatpansri@gmail.com>
2024-11-29 01:39:11 +0700

    Add files via upload

---
+++ HW6-5.cpp
@@ -0,0 +1,42 @@
+/*
+    รับค่าจากผู้ใช้จำนวน 1 ค่า และให้แสดงจำนวนเฉพาะตั้งแต่ค่าแรก จนถึงค่าที่ผู้ใช้กรอก โดยแสดงจำนวนเฉพาะจาก "มาก ไปหา น้อย"
+    (โจทย์ข้อนี้ให้ใช้ For Loop เท่านั้น)
+    
+    Test case:
+        Enter number :
+            10
+    Output:
+        7 5 3 2
+
+    Test case:
+        Enter number :
+            100
+    Output:
+        97 89 83 79 73 71 67 61 59 53 47 43 41 37 31 29 23 19 17 13 11 7 5 3 2
+*/
+
+#include <stdio.h>
+
+int isPrime(int n) {
+    if (n < 2) return 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int number;
+    printf("Enter number:\n");
+    scanf("%d", &number);
+
+    printf("Output:\n");
+    for (int i = number; i >= 2; i--) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
