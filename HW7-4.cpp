8282b402550faacd3a5c65c4d17711d6f246c4b9
NatTHboi <supanatpansri@gmail.com>
2024-11-30 00:22:34 +0700

    Add files via upload

---
+++ HW7-4.cpp
@@ -0,0 +1,54 @@
+/*
+    รับค่าจากผู้ใช้จำนวน 1 ค่า และให้แสดงจำนวนเฉพาะตั้งแต่ค่าแรก จนถึงค่าที่ผู้ใช้กรอก โดยแสดงจำนวนเฉพาะจาก "มาก ไปหา น้อย"
+    (โจทย์ข้อนี้ให้ใช้ Do While Loop เท่านั้น)
+    
+    Test case:
+        Enter number :
+            10
+    Output:
+        7 5 3 2
+
+    Test case:
+        Enter number :
+            100
+    Output:
+        97 89 83 79 73 71 67 61 59 53 47 43 41 37 31 29 23 19 17 13 11 7 5 3 2
+*/
+
+#include <stdio.h>
+
+int is_prime(int num) {
+    if (num < 2) {
+        return 0;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    if (n < 2) {
+        printf("No prime numbers in this range.\n");
+        return 0;
+    }
+
+    printf("Output: ");
+    int current = n;
+    do {
+        if (is_prime(current)) {
+            printf("%d ", current);
+        }
+        current--;
+    } while (current >= 2);
+
+    printf("\n");
+    return 0;
+}
