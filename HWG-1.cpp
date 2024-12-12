b8cf9f5213fb0df77c56977ef9c98ecb61f55a42
NatTHboi <supanatpansri@gmail.com>
2024-12-13 00:58:40 +0700

    Add files via upload

---
+++ HWG-1.cpp
@@ -0,0 +1,32 @@
+/*
+    จงเขียนโปรแกรมเพื่อสร้างฟังก์ชัน SequentialSearch ให้สมบูรณ์
+
+    Test case:
+
+    Output:
+        Found in 3
+
+*/
+
+#include <stdio.h>
+
+int SequentialSearch( int data[], int n, int find ) ;
+
+int main() {
+    int RealData[ 6 ] = { 7, 4, 1, 8, 3, 2 } ;
+    printf( "Found in %d\n", SequentialSearch( RealData, 6, 8 ) ) ;
+    return 0 ;
+}
+
+int SequentialSearch( int data[], int n, int find ) {
+    int Output = -1 ;
+    
+    for (int i = 0; i < n; i++) {
+        if (data[i] == find) {
+            Output = i;
+            break;
+        }
+    }
+
+    return Output ;
+}
